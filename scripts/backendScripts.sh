ca17b1d955bd33b747e627cfac2b3fc25c34e0c1
Waleed Alsafari <105042752+WaleedAlsafari@users.noreply.github.com>
2025-08-11 22:28:01 +0300

    Update backendScripts.sh

--- scripts/backendScripts.sh
+++ scripts/backendScripts.sh
@@ -1,2 +1,18 @@
 sudo apt update && sudo apt upgrade -y
 
+sudo apt install -y nodejs git curl
+
+git clone https://github.com/WaleedAlsafari/Scalable-Cloud-Based-To-Do-App-with-Multi-VM-Architecture.git
+
+cd Scalable-Cloud-Based-To-Do-App-with-Multi-VM-Architecture/app/server
+
+cat <<EOF > .env
+DB_HOST=10.0.2.4
+DB_PORT=5432
+DB_USER=admin
+DB_PASSWORD=Admin123@
+DB_NAME=todoapp
+EOF
+
+npm install
+npm run server
