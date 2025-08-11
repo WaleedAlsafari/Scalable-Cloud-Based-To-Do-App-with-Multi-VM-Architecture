3e9d0a7186a0a22b43a1cb9be915a3fe6ffae1d4
Waleed Alsafari <105042752+WaleedAlsafari@users.noreply.github.com>
2025-08-11 22:15:59 +0300

    Update frontendScripts.sh

--- scripts/frontendScripts.sh
+++ scripts/frontendScripts.sh
@@ -3,8 +3,8 @@ sudo apt update && sudo apt upgrade -y
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install -y nodejs git
 
-git clone https://github.com/WaleedAlsafari/Scalable-Cloud-Based-To-Do-App-with-Multi-VM-Architecture/tree/main/app
-cd PERN-ToDo-App/client
+git clone https://github.com/WaleedAlsafari/Scalable-Cloud-Based-To-Do-App-with-Multi-VM-Architecture.git
+cd Scalable-Cloud-Based-To-Do-App-with-Multi-VM-Architecture.git/app/client
 
 
 npm install
