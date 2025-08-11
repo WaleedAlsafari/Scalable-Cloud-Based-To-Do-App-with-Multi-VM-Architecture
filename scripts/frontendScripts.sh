7f86ed89e413666b630f2ce215c66f580927c3ee
Waleed Alsafari <105042752+WaleedAlsafari@users.noreply.github.com>
2025-08-11 22:18:58 +0300

    Update frontendScripts.sh

--- scripts/frontendScripts.sh
+++ scripts/frontendScripts.sh
@@ -4,7 +4,7 @@ curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install -y nodejs git
 
 git clone https://github.com/WaleedAlsafari/Scalable-Cloud-Based-To-Do-App-with-Multi-VM-Architecture.git
-cd Scalable-Cloud-Based-To-Do-App-with-Multi-VM-Architecture.git/app/client
+cd Scalable-Cloud-Based-To-Do-App-with-Multi-VM-Architecture/app/client
 
 
 npm install
