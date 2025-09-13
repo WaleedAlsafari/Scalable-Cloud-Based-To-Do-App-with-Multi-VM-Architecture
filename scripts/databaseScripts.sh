8faa7517e8d7301752c2bd1dd5f95917ce48c43c
Waleed Alsafari <105042752+WaleedAlsafari@users.noreply.github.com>
2025-09-13 20:26:58 +0300

    Update databaseScripts.sh

--- scripts/databaseScripts.sh
+++ scripts/databaseScripts.sh
@@ -6,7 +6,7 @@ sudo -i -u postgres psql -c "CREATE DATABASE todoapp;"
 sudo -i -u postgres psql -c "CREATE USER admin WITH ENCRYPTED PASSWORD 'Admin123@';"
 sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE todoapp TO myuser;"
 
-echo "host    all             all             10.0.0.0/24           md5" | sudo tee -a /etc/postgresql/*/main/pg_hba.conf
+echo "host    all             all             10.0.1.0/24           md5" | sudo tee -a /etc/postgresql/*/main/pg_hba.conf
 
 sudo sed -i "s/^#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/*/main/postgresql.conf
 
